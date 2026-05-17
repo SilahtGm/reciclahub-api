c3231facf7e0a05a6d5b8b8785d4c289be348246
Thalis Sousa <tatagamer677@gmail.com>
2026-05-17 14:31:28 -0300

    inicializando projeto

---
+++ src/main/java/br/com/reciclahub/ReciclahubApplication.java
@@ -0,0 +1,14 @@
+package br.com.reciclahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciclahubApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ReciclahubApplication.class, args);
+	}
+
+}
